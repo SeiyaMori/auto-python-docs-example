261fecc436eb49b85d80496b50b1e4354fc692ae
seiyamori <seiyamori95@gmail.com>
2024-02-27 21:08:53 +0000

    Fixed html theme

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon'
 ]
-templates_path = ['sphinx_rtd_theme']
+templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
@@ -31,5 +31,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
